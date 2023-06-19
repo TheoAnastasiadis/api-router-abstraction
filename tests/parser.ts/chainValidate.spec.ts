eb8c16e8733eb4220c2391bb2eed8e6964f48d80
Teo Anastasiadis <theoanastasiadis98@gmail.com>
2023-06-19 15:20:26 +0300

    chain validate tests

--- tests/parser.ts/chainValidate.spec.ts
+++ tests/parser.ts/chainValidate.spec.ts
@@ -1,19 +1,20 @@
-import { ConsumedRequest, RequestT } from "../../src/parser/request"
+import { RequestT } from "../../src/parser/request"
 import { chainValidate } from "../../src/parser/chainValidate"
 import { bodyRegistry } from "../../src/validators/body"
 import { authRegistry } from "../../src/validators/auth"
 import { Validator } from "../../src/validators"
 
 describe("chainValidate", () => {
-    it("", () => {
-        const request: RequestT = {
-            path: "/posts/3?incognito=true",
-            method: "GET",
-        }
-        const previousValidation = { ...request, consumed: {}, healthy: true }
-        const bodyRegistry: bodyRegistry = {}
-        const authRegistry: authRegistry = {}
-        const crntIdx = 0
+    const request: RequestT = {
+        path: "/posts/3?incognito=true",
+        method: "GET",
+    }
+    const previousValidation = { ...request, consumed: {}, healthy: true }
+    const bodyRegistry: bodyRegistry = {}
+    const authRegistry: authRegistry = {}
+    const crntIdx = 0
+
+    it("consumed matching requests", () => {
         const validators: Validator<
             typeof bodyRegistry,
             typeof authRegistry
@@ -47,6 +48,47 @@ describe("chainValidate", () => {
                 method: "GET",
                 consumed: { id: 3 },
                 healthy: true,
+            },
+            nextIdx: 2,
+        })
+    })
+
+    it("consumes non matching requests", () => {
+        const validators: Validator<
+            typeof bodyRegistry,
+            typeof authRegistry
+        >[] = ["/posts", "/:onlyNew(boolean)"]
+        const result1 = chainValidate(
+            previousValidation,
+            validators[0],
+            bodyRegistry,
+            authRegistry,
+            crntIdx
+        )
+        expect(result1).toEqual({
+            consumedRequest: {
+                path: "/3?incognito=true",
+                method: "GET",
+                consumed: {},
+                healthy: true,
+            },
+            nextIdx: 1,
+        })
+        const result2 = chainValidate(
+            result1.consumedRequest,
+            validators[1],
+            bodyRegistry,
+            authRegistry,
+            result1.nextIdx
+        )
+        expect(result2).toEqual({
+            consumedRequest: {
+                path: "?incognito=true",
+                method: "GET",
+                consumed: {
+                    onlyNew: false,
+                },
+                healthy: false,
             },
             nextIdx: 2,
         })
